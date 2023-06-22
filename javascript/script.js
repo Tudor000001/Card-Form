d694575636927adc54b6080c7750f9e7dae0a0fe
w w <lupsetudor@gmail.com>
2023-06-22 23:55:30 +0300

    da

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+var numeInput = document.getElementById('nome');
+var locNume = document.getElementById('noome');
+
+numeInput.addEventListener('keypress', function () {
+    locNume.textContent = numeInput;                        ////NU MERE NUME PE CARD NIGGA
+});
