3d96b38c25e1a396e3ce73463551121c8fbaf348
w w <lupsetudor@gmail.com>
2023-06-24 00:46:48 +0300

    ComitareSmce

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,58 @@
 var numeInput = document.getElementById('nome');
 var locNume = document.getElementById('noome');
+var cardNrInput = document.getElementById('cardNrInput');
+var cardNr = document.getElementById('cardNr');
+var mmInput = document.getElementById('mm');
+var yyInput = document.getElementById('yy');
+var mmCard = document.getElementById('mmCard');
+var yyCard = document.getElementById('yyCard');
+var cvcInput = document.getElementById('cvcInput');
+var cvcCard = document.getElementById('cvcCard');
+var formular = document.getElementById('formular');
 
-numeInput.addEventListener('keypress', function () {
-    locNume.textContent = numeInput;                        ////NU MERE NUME PE CARD NIGGA
+
+numeInput.addEventListener('keyup', function () {
+    locNume.innerHTML = numeInput.value;
+});
+
+var nr = 0;
+function addSpacesEveryFourCharacters(str) {
+    return str.replace(/(.{4})/g, '$1 ');
+}
+
+
+cardNrInput.addEventListener('keyup', function () {
+    cardNr.innerHTML = addSpacesEveryFourCharacters(cardNrInput.value);
+});
+
+const telInput = document.querySelector('input[type="tel"]');
+
+mmInput.addEventListener('keyup', function () {
+    mmCard.innerHTML = mmInput.value;
+});
+
+yyInput.addEventListener('keyup', function () {
+    yyCard.innerHTML = yyInput.value;
+});
+
+cvcInput.addEventListener('keyup', function () {
+    cvcCard.innerHTML = cvcInput.value;
+});
+
+const container = document.getElementById('formular'); // Replace 'container' with the ID of the parent element containing the input fields
+
+container.addEventListener('keydown', function (event) {
+    const target = event.target;
+    const key = event.key;
+    const isLetter = /[a-zA-Z]/.test(key);
+    const isBackspace = key === 'Backspace';
+
+    if (target.tagName === 'INPUT' && target.type !== 'text' && isLetter && !isBackspace) {
+        event.preventDefault();
+    }
+});
+
+formular.addEventListener('submit', function (event) {
+    event.preventDefault();
+    formular.style.display = 'none';
 });
